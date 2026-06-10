b26c7367c45af5e6e892624adf7607ad538d387b
AndreRoj <130945779+AndreRoj@users.noreply.github.com>
2026-06-10 19:47:20 -0400

    Escenario ambientado para la cocina

--- Assets/Scripts/ControladorJugador.cs
+++ Assets/Scripts/ControladorJugador.cs
@@ -7,11 +7,13 @@ public class KinematicMovement : MonoBehaviour
     public float turnSpeed = 10f;
     
     private CharacterController _controller;
+    //private Animator _animator;
     private Matrix4x4 _isoMatrix = Matrix4x4.Rotate(Quaternion.Euler(0, 45, 0));
 
     void Start()
     {
         _controller = GetComponent<CharacterController>();
+        //_animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -27,9 +29,12 @@ public class KinematicMovement : MonoBehaviour
         // Crea una matriz que rota -90 grados en el eje Y
         _isoMatrix = Matrix4x4.Rotate(Quaternion.Euler(0, -90, 0));
 
+
         // Transformamos el input para que coincida con la cámara isométrica (45°)
         Vector3 skewedInput = _isoMatrix.MultiplyPoint3x4(input.normalized);
-        
+
+        //Magnitud del moviento al Animator
+        //_animator.SetFloat("Speed", input.magnitude);
         //Vector3 skewedInput = input.normalized; // Sin transformación para movimiento directo
 
         if (input != Vector3.zero)
@@ -42,4 +47,13 @@ public class KinematicMovement : MonoBehaviour
             transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, turnSpeed * Time.deltaTime);
         }
     }
+
+    //void CheckedInteraction()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.E))
+    //    {
+    //        // Aquí puedes implementar la lógica para interactuar con objetos cercanos
+    //        Debug.Log("Interacción activada");
+    //    }
+    //}
 }
