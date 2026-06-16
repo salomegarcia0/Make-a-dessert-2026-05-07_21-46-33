8184f651afe5916ee4624a6b891b5f87ce30d7a2
Salomé García <salome.garcia@correo.unimet.edu.ve>
2026-06-16 00:51:33 -0400

    adelanto del delivery manager

---
+++ Assets/Scripts/DeliveryManager.cs
@@ -0,0 +1,10 @@
+using NUnit.Framework;
+using UnityEngine;
+
+public class RecipeManager : MonoBehaviour
+{
+
+    private List<List<IngredientObject>> recipeList;
+
+
+}
---
+++ Assets/Scripts/RecipeSO.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[CreateAssetMenu()]
+public class RecipeSO: ScriptableObject
+{
+
+	public List<IngredientObject> IngredientObjectList;
+	public string RecipeName;
+
+}
